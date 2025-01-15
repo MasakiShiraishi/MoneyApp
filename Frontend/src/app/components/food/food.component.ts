6c60b8959bf71b95481247abe600cdbebd01d2d5
Masaki Shiraishi <masaki.shiraishi83@gmail.com>
2025-01-15 19:52:31 +0100

    import FoodTransactionsComponent to food.comooents

--- Frontend/src/app/components/food/food.component.ts
+++ Frontend/src/app/components/food/food.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ArrowToCashbookComponent } from "../arrow-to-cashbook/arrow-to-cashbook.component";
+import { FoodTransactionsComponent } from "../food-transactions/food-transactions.component";
 
 @Component({
   selector: 'app-food',
   standalone: true,
-  imports: [ArrowToCashbookComponent],
+  imports: [ArrowToCashbookComponent, FoodTransactionsComponent],
   templateUrl: './food.component.html',
   styleUrl: './food.component.css'
 })
