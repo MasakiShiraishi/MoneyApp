cfb4e96dd4eee2b922e02f4658609ce4ef94d7c2
Masaki Shiraishi <masaki.shiraishi83@gmail.com>
2025-01-15 19:07:00 +0100

    added standalone to arrow-to-cashbook.ts

--- Frontend/src/app/components/arrow-to-cashbook/arrow-to-cashbook.component.ts
+++ Frontend/src/app/components/arrow-to-cashbook/arrow-to-cashbook.component.ts
@@ -3,6 +3,7 @@ import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-arrow-to-cashbook',
+  standalone: true,
   imports: [RouterModule],
   templateUrl: './arrow-to-cashbook.component.html',
   styleUrl: './arrow-to-cashbook.component.css'
