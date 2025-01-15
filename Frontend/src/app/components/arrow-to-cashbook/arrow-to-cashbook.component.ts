655f164b0cf17a8561105e598d95292bedb60d0e
Masaki Shiraishi <masaki.shiraishi83@gmail.com>
2025-01-15 19:00:54 +0100

    created arrow-to-cashbook component

---
+++ Frontend/src/app/components/arrow-to-cashbook/arrow-to-cashbook.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-arrow-to-cashbook',
+  imports: [RouterModule],
+  templateUrl: './arrow-to-cashbook.component.html',
+  styleUrl: './arrow-to-cashbook.component.css'
+})
+export class ArrowToCashbookComponent {
+
+}
