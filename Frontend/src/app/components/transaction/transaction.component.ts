239a963646a65ec7554b296e874cfc954ac54828
Masaki Shiraishi <masaki.shiraishi83@gmail.com>
2025-01-15 18:27:31 +0100

    added skelton function to transaction.html

--- Frontend/src/app/components/transaction/transaction.component.ts
+++ Frontend/src/app/components/transaction/transaction.component.ts
@@ -1,15 +1,30 @@
 import { Component } from '@angular/core';
 import { ArrowToHomeComponent } from '../arrow-to-home/arrow-to-home.component';
-import { TransactionListComponent } from "../transaction-list/transaction-list.component";
+import { TransactionListComponent } from '../transaction-list/transaction-list.component';
 import { HttpClientModule } from '@angular/common/http';
+import { TransactionGroupSkeletonComponent } from '../skeleton/transaction-group-skeleton/transaction-group-skeleton.component';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-transaction',
   standalone: true,
-  imports: [ArrowToHomeComponent, TransactionListComponent, HttpClientModule],
+  imports: [
+    ArrowToHomeComponent,
+    TransactionListComponent,
+    HttpClientModule,
+    TransactionGroupSkeletonComponent,
+    CommonModule,
+  ],
   templateUrl: './transaction.component.html',
-  styleUrl: './transaction.component.css'
+  styleUrl: './transaction.component.css',
 })
 export class TransactionComponent {
+  isLoading = true;
 
+  ngOnInit() {
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 2000);
+  }
 }
