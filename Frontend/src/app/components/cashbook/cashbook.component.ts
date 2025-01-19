1d990dd4ff0953bfd13a83412599f4f06ea0a7f8
Masaki Shiraishi <masaki.shiraishi83@gmail.com>
2025-01-19 16:42:58 +0100

    import CalculateAccommodationComponent

--- Frontend/src/app/components/cashbook/cashbook.component.ts
+++ Frontend/src/app/components/cashbook/cashbook.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ArrowToHomeComponent } from '../arrow-to-home/arrow-to-home.component';
 import { CalculateFoodComponent } from "../calculates/calculate-food/calculate-food.component";
+import { CalculateAccommodationComponent } from "../calculates/calculate-accommodation/calculate-accommodation.component";
 
 @Component({
   selector: 'app-cashbook',
   standalone: true,
-  imports: [ArrowToHomeComponent, CalculateFoodComponent],
+  imports: [ArrowToHomeComponent, CalculateFoodComponent, CalculateAccommodationComponent],
   templateUrl: './cashbook.component.html',
   styleUrl: './cashbook.component.css'
 })
