a4a4261e96114dde6de83bddd24cc26dc8768da1
Masaki Shiraishi <masaki.shiraishi83@gmail.com>
2025-01-20 18:29:52 +0100

    improve transaction-list components so that they can be reused

--- Frontend/src/app/components/transaction-list/transaction-list.component.ts
+++ Frontend/src/app/components/transaction-list/transaction-list.component.ts
@@ -1,35 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
 import { Transaction } from '../../models/transaction';
 import { TransactionService } from '../../services/transaction.service';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { TransactionGroupSkeletonComponent } from '../skeleton/transaction-group-skeleton/transaction-group-skeleton.component';
 
 @Component({
   selector: 'app-transaction-list',
   standalone: true,
-  imports: [CommonModule, HttpClientModule],
+  imports: [CommonModule, HttpClientModule, TransactionGroupSkeletonComponent],
   templateUrl: './transaction-list.component.html',
   styleUrl: './transaction-list.component.css',
 })
 export class TransactionListComponent implements OnInit {
+  @Input() category: string = '';
+  @Input() subCategories: string[] = [];
   transactions: Transaction[] = [];
   lastDate: string | null = null;
+  isLoading = true;
 
-  constructor(private transactionService: TransactionService) {}
+  constructor(private transactionService: TransactionService, private cdr: ChangeDetectorRef) {}
 
   ngOnInit(): void {
+    this.fetchTransactions();
+  }
+
+  setTimeouts(): void {
+    setTimeout(() => {
+      this.isLoading = false;
+      this.cdr.detectChanges();
+    }, 1500);
+
+  }
+
+  fetchTransactions(): void {
     this.transactionService.getTransactions().subscribe(
       (data) => {
-        this.transactions = data;
+        this.transactions = data
+          .filter(
+            (transaction) =>
+              !this.category ||
+              transaction.category === this.category ||
+              this.subCategories.includes(transaction.category)
+          )
+          .sort((a, b) => {
+            return new Date(b.date).getTime() - new Date(a.date).getTime();
+          });
+         this.setTimeouts();
       },
       (error) => {
         console.error('Error fetching transactions:', error);
+        this.setTimeouts();
       }
     );
   }
 
   shouldShowDate(currentDate: string, lastDate: string | null): boolean {
-    return currentDate !== lastDate;
+    if (currentDate !== lastDate) {
+      this.lastDate = currentDate;
+      return true;
+    }
+    return false;
   }
   formatDate(date: string): string {
     const options: Intl.DateTimeFormatOptions = {
--- Frontend/src/app/components/transaction/transaction.component.ts
+++ Frontend/src/app/components/transaction/transaction.component.ts
@@ -2,9 +2,7 @@ import { Component } from '@angular/core';
 import { ArrowToHomeComponent } from '../arrow-to-home/arrow-to-home.component';
 import { TransactionListComponent } from '../transaction-list/transaction-list.component';
 import { HttpClientModule } from '@angular/common/http';
-import { TransactionGroupSkeletonComponent } from '../skeleton/transaction-group-skeleton/transaction-group-skeleton.component';
 import { CommonModule } from '@angular/common';
-
 
 @Component({
   selector: 'app-transaction',
@@ -13,18 +11,9 @@ import { CommonModule } from '@angular/common';
     ArrowToHomeComponent,
     TransactionListComponent,
     HttpClientModule,
-    TransactionGroupSkeletonComponent,
     CommonModule,
   ],
   templateUrl: './transaction.component.html',
   styleUrl: './transaction.component.css',
 })
-export class TransactionComponent {
-  isLoading = true;
-
-  ngOnInit() {
-    setTimeout(() => {
-      this.isLoading = false;
-    }, 2000);
-  }
-}
+export class TransactionComponent {}
