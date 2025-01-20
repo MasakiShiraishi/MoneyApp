5bb5c2d1693347e06b8a3d1b4ae1537b3f592427
Masaki Shiraishi <masaki.shiraishi83@gmail.com>
2025-01-20 10:21:30 +0100

    created transport-transactions component

---
+++ Frontend/src/app/components/cashbook-transactions/transport-transactions/transport-transactions.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transport-transactions',
+  imports: [],
+  templateUrl: './transport-transactions.component.html',
+  styleUrl: './transport-transactions.component.css'
+})
+export class TransportTransactionsComponent {
+
+}
