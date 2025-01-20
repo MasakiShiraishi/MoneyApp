7381a40dd5f66c1155223d571bdbe45bf3ba5074
Masaki Shiraishi <masaki.shiraishi83@gmail.com>
2025-01-20 18:38:25 +0100

    updated to create transaction-list component

--- Frontend/src/app/components/cashbook-transactions/accommodation-transactions/accommodation-transactions.component.ts
+++ Frontend/src/app/components/cashbook-transactions/accommodation-transactions/accommodation-transactions.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { TransactionListComponent } from '../../transaction-list/transaction-list.component';
+import { HeaderToCashbookComponent } from '../header-to-cashbook/header-to-cashbook.component';
 
 @Component({
   selector: 'app-accommodation-transactions',
-  imports: [],
-  templateUrl: './accommodation-transactions.component.html',
-  styleUrl: './accommodation-transactions.component.css'
+  standalone: true,
+  imports: [TransactionListComponent, HeaderToCashbookComponent],
+  template: `
+    <app-header-to-cashbook [category]="category"></app-header-to-cashbook>
+    <app-transaction-list
+      [category]="category"
+      [subCategories]="accommodationSubCategories"
+    ></app-transaction-list>
+  `,
+  styleUrl: './accommodation-transactions.component.css',
 })
 export class AccommodationTransactionsComponent {
-
+  category: string = 'accommodation';
+  accommodationSubCategories: string[] = [
+    'health',
+    'rent',
+    'internet',
+    'utilities',
+  ];
 }
