0d033cf094f50f4b7516271946bb9da039b31b81
Masaki Shiraishi <masaki.shiraishi83@gmail.com>
2025-01-20 18:33:48 +0100

    updated cashbook directory files after imporve transaction-list component

--- Frontend/src/app/components/cashbook-transactions/clothe-transactions/clothe-transactions.component.ts
+++ Frontend/src/app/components/cashbook-transactions/clothe-transactions/clothe-transactions.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { TransactionListComponent } from '../../transaction-list/transaction-list.component';
+import { HeaderToCashbookComponent } from '../header-to-cashbook/header-to-cashbook.component';
 
 @Component({
   selector: 'app-clothe-transactions',
-  imports: [],
-  templateUrl: './clothe-transactions.component.html',
-  styleUrl: './clothe-transactions.component.css'
+  imports: [TransactionListComponent, HeaderToCashbookComponent],
+  template: `
+    <app-header-to-cashbook [category]="category"></app-header-to-cashbook>
+    <app-transaction-list
+      [category]="category"
+      [subCategories]="clothesSubCategories"
+    ></app-transaction-list>
+  `,
+  styleUrl: './clothe-transactions.component.css',
 })
 export class ClotheTransactionsComponent {
-
+  category: string = 'clothes';
+  clothesSubCategories: string[] = ['shoes', 'accessories', 'clothes'];
 }
