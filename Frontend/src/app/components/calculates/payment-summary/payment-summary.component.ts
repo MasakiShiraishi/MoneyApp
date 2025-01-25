140972ae71e0685898ac98b1998466b75b37f523
Masaki Shiraishi <masaki.shiraishi83@gmail.com>
2025-01-25 07:49:37 +0100

    added currencyPipe function

--- Frontend/src/app/components/calculates/payment-summary/payment-summary.component.ts
+++ Frontend/src/app/components/calculates/payment-summary/payment-summary.component.ts
@@ -1,32 +1,43 @@
 import { Component } from '@angular/core';
 import { Transaction } from '../../../models/transaction';
 import { TransactionService } from '../../../services/transaction.service';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-payment-summary',
   imports: [],
   templateUrl: './payment-summary.component.html',
-  styleUrl: './payment-summary.component.css'
+  styleUrl: './payment-summary.component.css',
+  providers: [CurrencyPipe],
 })
 export class PaymentSummaryComponent {
   transactions: Transaction[] = [];
-  totalByPayment: {[key: string]: number} = {};
+  totalByPayment: { [key: string]: number } = {};
 
-  constructor(private transactionService: TransactionService) { }
+  constructor(
+    private transactionService: TransactionService,
+    private currencyPipe: CurrencyPipe
+  ) {}
 
-  ngOnInit(): void{
+  ngOnInit(): void {
     this.fetchPaymentSummary();
   }
 
   fetchPaymentSummary(): void {
     this.transactionService.getTransactions().subscribe(
       (data: Transaction[]) => {
-      this.transactions = data;
-      this.totalByPayment = this.transactionService.calculateTotalByPayment(this.transactions);
-    },
-    (error) => {
-      console.error('Error fetching transactions',error);
-    }
-  );
+        this.transactions = data;
+        this.totalByPayment = this.transactionService.calculateTotalByPayment(this.transactions);
+      },
+      (error) => {
+        console.error('Error fetching transactions', error);
+      }
+    );
   }
+  getFormattedAmount(amount: number): string {
+    const negativeAmount = -amount;
+    const formattedAmount = this.currencyPipe.transform(negativeAmount, 'SEK', 'symbol', '1.0-0');
+    return formattedAmount ? formattedAmount.replace('SEK', '').trim() : '0';
+  }
+  
 }
