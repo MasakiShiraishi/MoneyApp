07933c46d0c905425f37f296b2bb5516788953ea
Masaki Shiraishi <masaki.shiraishi83@gmail.com>
2025-01-25 08:48:56 +0100

    import AccountInfoComponent

--- Frontend/src/app/components/account/account.component.ts
+++ Frontend/src/app/components/account/account.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ArrowToHomeComponent } from '../arrows/arrow-to-home/arrow-to-home.component';
+import { AccountInfoComponent } from "./account-info/account-info.component";
 
 @Component({
   selector: 'app-account',
   standalone: true,
-  imports: [ArrowToHomeComponent],
+  imports: [ArrowToHomeComponent, AccountInfoComponent],
   templateUrl: './account.component.html',
   styleUrl: './account.component.css'
 })
