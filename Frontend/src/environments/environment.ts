c76cfa631412269da82e442e1672a0ea3e07f5f6
Masaki Shiraishi <masaki.shiraishi83@gmail.com>
2025-02-04 19:55:35 +0100

    changed api addres to local environment

--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://moneyapp-hyhxc2f6bkezcve3.germanywestcentral-01.azurewebsites.net/api/',
+  apiUrl: 'http://localhost:5234/api/',
 };
