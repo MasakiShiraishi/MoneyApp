b791940bdc47f598fd73cce719aafa3b0fe3ec36
Masaki Shiraishi <masaki.shiraishi83@gmail.com>
2025-02-08 11:14:35 +0100

    changed apiURL

--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5234/api/',
-  // apiUrl: 'https://moneyapp-hyhxc2f6bkezcve3.germanywestcentral-01.azurewebsites.net/api/',
+  // apiUrl: 'http://localhost:5234/api/',
+  apiUrl: 'https://moneyapp-hyhxc2f6bkezcve3.germanywestcentral-01.azurewebsites.net/api/',
 };
